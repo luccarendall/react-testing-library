607c0d2836789f6c91b7578acfd2c9b83029e2a5
Lucca Rendall <luccarendall1@gmail.com>
2022-03-24 15:45:12 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,16 +1,16 @@
 // npx stryker run ./stryker/nomeDoArquivo.conf.json
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from './aux/renderWithRouter';
 
-describe('Testes do componente <App.js />', () => {
+describe('Testes do componente App', () => {
   test('Testa se o topo da aplicação contém um conjunto fixo de links de navegação',
     () => {
       // Acessando elementos na tela
       // Dica: Usar  {name: /exemplo/i} para cancelar o case sensitive. Se o avaliador der problema eu coloco.
-      render(<App />);
+      renderWithRouter(<App />);
       const home = screen.getByRole('link', { name: 'Home' });
       const about = screen.getByRole('link', { name: 'About' });
       const favoritePokemons = screen.getByRole('link', { name: 'Favorite Pokémons' });
@@ -21,17 +21,35 @@ describe('Testes do componente <App.js />', () => {
       expect(favoritePokemons).toBeInTheDocument();
     });
 
-  test('Testa se a aplicação redireciona para a página inicial ao clicar no link Home',
+  test('Testa se a aplicação redireciona p/ a página inicial ao clicar no link Home',
     () => {
       const { history } = renderWithRouter(<App />);
       const home = screen.getByRole('link', { name: 'Home' });
       userEvent.click(home);
       expect(history.location.pathname).toBe('/');
     });
+
+  test('Testa se a aplicação redireciona p/ a página About ao clicar no link About',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      const about = screen.getByRole('link', { name: 'About' });
+      userEvent.click(about);
+      expect(history.location.pathname).toBe('/about');
+    });
+
+  test('Testa se a aplicação redireciona p/ Poke Favoritados ao clicar no link favorites',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      const favorite = screen.getByRole('link', { name: 'Favorite Pokémons' });
+      userEvent.click(favorite);
+      expect(history.location.pathname).toBe('/favorites');
+    });
+
+  test('Testa se a aplicação redireciona p/ Not Found ao entrar em uma URL desconhecida.',
+    () => {
+      const { history } = renderWithRouter(<App />);
+      history.push('/notFound');
+      const notfound = screen.getByText('Page requested not found');
+      expect(notfound).toBeDefined();
+    });
 });
-
-// test('', () => {});
-
-// test('', () => {});
-
-// test('', () => {});
